23dbdc4b4566b91fc877f909703c8d2efb733b39
Baris Dogu <baris.dogu@mokaunited.com>
2025-10-05 13:16:04 +0300

    ...

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Channel struct {
 }
 
 var channels = []Channel{
-	{Name: "TRT 1", Slug: "trt1", URL: "https://tv-trt1.medya.trt.com.tr/master_1080.m3u8"},
+	{Name: "Selcuk Sports", Slug: "selcuk_sports", URL: "https://df16ea90s1u1080.05949e7ffb6b0bf5.live/live/selcukobs1/playlist.m3u8"},
 	{Name: "Tabii Spor 1", Slug: "tabii_spor_1", URL: "https://beert7sqimrk0bfdupfgn6qew.medya.trt.com.tr/master_1080p.m3u8"},
 	{Name: "Tabii Spor 2", Slug: "tabii_spor_2", URL: "https://klublsslubcgyiz7zqt5bz8il.medya.trt.com.tr/master_1080p.m3u8"},
 	{Name: "Tabii Spor 3", Slug: "tabii_spor_3", URL: "https://ujnf69op16x2fiiywxcnx41q8.medya.trt.com.tr/master_1080p.m3u8"},
@@ -26,7 +26,7 @@ var channels = []Channel{
 	{Name: "Tabii Spor 5", Slug: "tabii_spor_5", URL: "https://z3mmimwz148csv0vaxtphqspf.medya.trt.com.tr/master_1080p.m3u8"},
 	{Name: "Tabii Spor 6", Slug: "tabii_spor_6", URL: "https://vbtob9hyq58eiophct5qctxr2.medya.trt.com.tr/master_1080p.m3u8"},
 	{Name: "Tabii Spor 7", Slug: "tabii_spor_7", URL: "https://rxne77juptdeyke3tytvgqwyh.medya.trt.com.tr/master_1080p.m3u8"},
-	{Name: "Selcuk Sports", Slug: "selcuk_sports", URL: "https://df16ea90s1u1080.05949e7ffb6b0bf5.live/live/selcukobs1/playlist.m3u8"},
+	{Name: "TRT 1", Slug: "trt1", URL: "https://tv-trt1.medya.trt.com.tr/master_1080.m3u8"},
 }
 
 //go:embed assets/index.html
